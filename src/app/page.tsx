1c8a4ff764c7349481d730288baa2123bd370c28
aliyan jabbar <jabbaraliyan805@gmail.com>
2025-01-14 12:22:48 +0500

    removing pages route and converting it into pages drop down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// "use client";
 import Home1 from "@/components/home/home1";
 import Home2 from "@/components/home/home2";
 import Home3 from "@/components/home/home3";
@@ -6,7 +7,133 @@ import Home5 from "@/components/home/home5";
 import Home6 from "@/components/home/home6";
 import Home7 from "@/components/home/home7";
 import Home8 from "@/components/home/home8";
+// import { useEffect, useState } from "react";
+// import { client } from "../sanity/lib/client";
+// import imageUrlBuilder from "@sanity/image-url";
+// import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 export default function Home() {
+  
+  //defining type of data
+  // type Data = {
+  //   id: number;
+  //   name: string;
+  //   image: string;
+  //   price: number;
+  //   rating: number;
+  //   quantity: number;
+  // };
+
+  //1. essential states for data fetching and inserting into sanity
+  // const [Products, setProducts] = useState<Data[] | null>(null);
+  // const [dataInserted, setDataInserted] = useState(false); // to ensure data is only inserted once
+  //2.fetching products data from api
+  // useEffect(() => {
+  //   // Function to fetch product data
+  //   async function fetchingProducts() {
+  //     try {
+  //       const baseUrl =
+  //         process.env.NEXT_PUBLIC_API_URL || process.env.VERCEL_URL;
+  //       const fetchedProducts = await fetch(`${baseUrl}/api/products`);
+
+  //       if (!fetchedProducts.ok) {
+  //         console.error("Fetch failed with status:", fetchedProducts.status);
+  //         throw new Error(`HTTP error! Status: ${fetchedProducts.status}`);
+  //       }
+
+  //       const response = await fetchedProducts.json();
+  //       // Log response to debug
+  //       console.log("Fetched Products:", response.data);
+  //       setProducts(response.data); // Set fetched products to state
+  //     } catch (error) {
+  //       console.error("Error during fetch:", error);
+  //     }
+  //   }
+
+  //   fetchingProducts();
+  // }, []);
+
+  //3.function for inserting data into sanity
+  // async function insertData() {
+  //   if (Products !== null && !dataInserted) {
+  //     try {
+  //       // Creating base documents to insert in sanity
+  //       const createdDocs = await Promise.all(
+  //         Products.map((product) =>
+  //           client.create({
+  //             _type: "product",
+  //             name: product.name,
+  //             price: product.price,
+  //             rating: product.rating,
+  //             quantity: product.quantity,
+  //           })
+  //         )
+  //       );
+
+  //       // Updating documents with images
+  //       const updatedDocs = await Promise.all(
+  //         createdDocs.map(async (doc, index) => {
+  //           // Firstly fetching the image
+  //           const response = await fetch(Products[index].image);
+  //           const blob = await response.blob();
+  //           const imageBuffer = await blob.arrayBuffer();
+
+  //           // Uploading image as buffer
+  //           const imageAsset = await client.assets.upload(
+  //             "image",
+  //             Buffer.from(imageBuffer)
+  //           );
+
+  //           return client
+  //             .patch(doc._id)
+  //             .set({
+  //               image: {
+  //                 _type: "image",
+  //                 asset: {
+  //                   _type: "reference",
+  //                   _ref: imageAsset._id,
+  //                 },
+  //               },
+  //             })
+  //             .commit();
+  //         })
+  //       );
+
+  //       setDataInserted(true);
+  //       console.log("Data with images successfully inserted:", updatedDocs);
+  //     } catch (err: any) {
+  //       console.error("Error inserting data:", err.message);
+  //     }
+  //   }
+  // }
+
+  //4.now inserting data in sanity using the above function
+  // useEffect(() => {
+  //   if (Products !== null && !dataInserted) {
+  //     insertData();
+  //   }
+  // }, [Products, dataInserted]); // Trigger when Products state changes
+
+
+
+  //5.fetching from sanity by groq query
+  // const query = `*[_type == "product"]{
+  //   _id,
+  //   name,
+  //   price,
+  //   rating,
+  //   quantity,
+  //   "imageUrl": image.asset->url
+  // }`;
+
+  // client
+  //   .fetch(query)
+  //   .then((data) => {
+  //     console.log("Fetched data:", data);
+  //   })
+  //   .catch((err) => {
+  //     console.error("Error fetching data:", err);
+  //   });
+
   return (
     <div className="bg-blackish">
       <Home1 />
--- src/components/topHeader.tsx
+++ src/components/topHeader.tsx
@@ -6,6 +6,8 @@ import searchIcon from "../../public/assets/icons/MagnifyingGlass-icon.png";
 import userIcon from "../../public/assets/icons/User-icon.png";
 import bagIcon from "../../public/assets/icons/Bag-icon.png";
 import { useCart } from "@/context/CartContext";
+import Link from "next/link";
+import { FaAngleDown } from "react-icons/fa";
 
 const TopHeader = () => {
   const { cart } = useCart();
@@ -13,6 +15,11 @@ const TopHeader = () => {
   const router = useRouter();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const [isPagesOpen, setIsPagesOpen] = useState(false);
+
+  const toggleIsPagesOpen = () => {
+    setIsPagesOpen(!isPagesOpen);
+  };
   const handleNavigation = (path: string) => {
     if (path !== pathname) {
       setIsMenuOpen(false);
@@ -24,11 +31,11 @@ const TopHeader = () => {
 
   return (
     <header className="text-white bg-blackish w-full body-font flex flex-wrap items-center z-20 px-[7%]">
-      <div className="w-full flex py-5 flex-col md:flex-row items-center z-20">
+      <div className="w-full flex py-5 flex-col min-[800px]:flex-row items-center z-20">
         {/* Logo */}
         <div
           onClick={() => handleNavigation("/")}
-          className="flex title-font font-medium items-center mb-4 md:mb-0 cursor-pointer"
+          className="flex title-font font-medium items-center mb-4 min-[800px]:mb-0 cursor-pointer"
         >
           <h1 className="ml-3 text-xl text-white font-sans text-[24px] font-bold z-20">
             Food<span className="text-orangeLike">tuck</span>
@@ -37,7 +44,7 @@ const TopHeader = () => {
 
         {/* Hamburger Icon */}
         <button
-          className="md:hidden ml-auto text-white focus:outline-none"
+          className="min-[800px]:hidden ml-auto text-white focus:outline-none"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           <svg
@@ -51,7 +58,9 @@ const TopHeader = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+              d={
+                isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"
+              }
             />
           </svg>
         </button>
@@ -60,35 +69,98 @@ const TopHeader = () => {
         <nav
           className={`${
             isMenuOpen ? "block" : "hidden"
-          } md:flex w-full overflow-x-auto md:overflow-visible md:mx-auto md:w-auto justify-center items-center text-base text-[14px] md:text-[16px] font-normal gap-2`}
+          } min-[800px]:flex w-full overflow-x-auto min-[800px]:overflow-visible min-[800px]:mx-auto min-[800px]:w-auto justify-center items-center text-base text-[14px] min-[800px]:text-[16px] font-normal gap-2`}
         >
-          <div className="flex flex-col items-center justify-center md:flex-row gap-2 md:gap-[2px] min-[780px]:gap-1 min-[820px]:gap-2 min-[900px]:gap-3 lg:gap-4 md:w-full flex-nowrap md:flex-wrap">
+          <div className="flex flex-col items-center justify-center min-[800px]:flex-row gap-2 md:gap-[2px] min-[780px]:gap-1 min-[820px]:gap-2 min-[900px]:gap-3 lg:gap-4 min-[800px]:w-full flex-nowrap min-[800px]:flex-wrap">
             {[
               { label: "Home", path: "/" },
               { label: "Menu", path: "/menu" },
               { label: "Blog", path: "/blogs" },
-              { label: "Pages", path: "/pages" },
+              { label: "Pages" },
               { label: "About", path: "/about" },
               { label: "Shop", path: "/shop" },
               { label: "Contact", path: "/contact" },
-            ].map((link) => (
-              <div
-                key={link.path}
-                onClick={() => handleNavigation(link.path)}
-                className={`px-2 py-1 cursor-pointer transition-all duration-150 whitespace-nowrap ${
-                  pathname === link.path
-                    ? "font-bold text-orangeLike"
-                    : "hover:text-orangeLike"
-                }`}
-              >
-                {link.label}
-              </div>
-            ))}
+            ].map((link) =>
+              link.label === "Pages" ? (
+                <div
+                  key={link.label}
+                  className="group relative"
+                  onClick={() => {
+                    toggleIsPagesOpen();
+                  }}
+                >
+                  <div className="flex gap-1 items-center justify-center px-2 py-1 cursor-pointer transition-all duration-150 whitespace-nowrap hover:text-orangeLike text-white lg:hover:fill-orangeLike">
+                    Pages
+                    <FaAngleDown />
+                  </div>
+                  <ul
+                    className={`absolute shadow-lg bg-transparent text-white space-y-3 lg:top-10 max-lg:top-12 -left-6 min-w-[250px] z-50 transition-all duration-300 ${
+                      isPagesOpen
+                        ? "opacity-100 visible translate-y-0"
+                        : "opacity-0 invisible -translate-y-2"
+                    } px-6 py-4`}
+                  >
+                    <li className="border-b py-2 ">
+                      <Link
+                        href="/notFound"
+                        className="hover:text-orangeLike transition text-txtBlack text-[15px] font-bold block"
+                      >
+                        Not Found Page
+                      </Link>
+                    </li>
+                    <li className="border-b py-2 ">
+                      <Link
+                        href="/userDetails"
+                        className="hover:text-orangeLike transition text-txtBlack text-[15px] font-bold block"
+                      >
+                        User Details
+                      </Link>
+                    </li>
+                    <li className="border-b py-2 ">
+                      <Link
+                        href="/faq"
+                        className="hover:text-orangeLike transition text-txtBlack text-[15px] font-bold block"
+                      >
+                        FAQ
+                      </Link>
+                    </li>
+                    <li className="border-b py-2 ">
+                      <Link
+                        href="shoppingCart"
+                        className="hover:text-orangeLike transition text-txtBlack text-[15px] font-bold block"
+                      >
+                        Cart
+                      </Link>
+                    </li>
+                    <li className="border-b py-2 ">
+                      <Link
+                        href="/checkout"
+                        className="hover:text-orangeLike transition text-txtBlack text-[15px] font-bold block"
+                      >
+                        Checkout
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <div
+                  key={link.path}
+                  onClick={() => handleNavigation(link.path!)}
+                  className={`px-2 py-1 cursor-pointer transition-all duration-150 whitespace-nowrap ${
+                    pathname === link.path
+                      ? "font-bold text-orangeLike"
+                      : "hover:text-orangeLike"
+                  }`}
+                >
+                  {link.label}
+                </div>
+              )
+            )}
           </div>
         </nav>
 
         {/* Icons */}
-        <div className="flex gap-3 sm:gap-5 mt-4 md:mt-0">
+        <div className="flex gap-3 sm:gap-5 mt-4 min-[800px]:mt-0">
           <div onClick={() => handleNavigation("/")} className="cursor-pointer">
             <Image
               src={searchIcon}
@@ -98,7 +170,10 @@ const TopHeader = () => {
               className="hover:-translate-y-1 transition-all duration-200"
             />
           </div>
-          <div onClick={() => handleNavigation("/userDetails")} className="cursor-pointer">
+          <div
+            onClick={() => handleNavigation("/userDetails")}
+            className="cursor-pointer"
+          >
             <Image
               src={userIcon}
               alt="user-icon"
@@ -107,7 +182,10 @@ const TopHeader = () => {
               className="hover:-translate-y-1 transition-all duration-200"
             />
           </div>
-          <div onClick={() => handleNavigation("/shoppingCart")} className="cursor-pointer">
+          <div
+            onClick={() => handleNavigation("/shoppingCart")}
+            className="cursor-pointer"
+          >
             <div className="relative hover:-translate-y-1 transition-all duration-200">
               <Image src={bagIcon} alt="bag-icon" width={24} height={24} />
               {cart.length > 0 && (
